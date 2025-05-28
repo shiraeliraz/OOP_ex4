12988d6e820533c1c62695a8737889d8cebfccc5
ronishpitzer <roni.shpitzer@mail.huji.ac.il>
2025-05-28 13:59:03 +0300

    starting the project

--- pepse/PepseGameManager.java
+++ pepse/PepseGameManager.java
@@ -1,6 +1,18 @@
 import danogl.GameManager;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import world.Sky;
 
 public class PepseGameManager extends GameManager {
+
+	@Override
+	public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
+		super.initializeGame(imageReader, soundReader, inputListener, windowController);
+		this.gameObjects().addGameObject(Sky.create(windowController.getWindowDimensions()));
+	}
+
 
 	public static void main (String[] args) {
 		PepseGameManager gameManager = new PepseGameManager();
--- pepse/world/Terrain.java
+++ pepse/world/Terrain.java
@@ -1,4 +1,33 @@
 package world;
 
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import util.ColorSupplier;
+
+import java.awt.*;
+import java.util.List;
+
 public class Terrain {
+	private final Vector2 windowDimensions;
+	private final int seed;
+	private float groundHeightAtX0;
+	private static final Color BASE_GROUND_COLOR = new Color(212, 123, 74);
+
+	public Terrain(Vector2 windowDimensions, int seed) {
+		this.windowDimensions = windowDimensions;
+		this.seed = seed;
+		this.groundHeightAtX0 = windowDimensions.y() * (float)2/3;
+	}
+
+	public float getGroundHeightAtX(float x) {
+		return groundHeightAtX0;
+	} // TODO: make more interesting
+
+	public List<Block> createInRange(int minX, int maxX) {
+		RectangleRenderable renderable = new RectangleRenderable(
+				ColorSupplier.approximateColor(BASE_GROUND_COLOR));
+		Block block = new Block()
+
+	}
 }
